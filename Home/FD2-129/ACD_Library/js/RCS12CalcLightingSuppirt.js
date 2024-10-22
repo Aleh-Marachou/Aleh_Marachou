41263f83531dd8d6e18b42f00c32a61d18870d25
Aleh_Marachou <oolem86@gmail.com>
2024-10-22 19:29:25 +0300

    wind

--- Home/FD2-129/ACD_Library/js/RCS12CalcLightingSuppirt.js
+++ Home/FD2-129/ACD_Library/js/RCS12CalcLightingSuppirt.js
@@ -1,5 +1,6 @@
 let supportMass = 0.981;
-let H = 10;
+let H = 9;
+let Dsupport = 0.20;
 let b_f = 1.2;
 let l_f = 1.2;
 let d_f = 2.6;
@@ -11,9 +12,15 @@ let q_q = 19.62;
 const const_c_dir = 1;
 const const_c_season = 1;
 const const_v_b0 = 23;
+const const_z_0 = 0.3;
 const const_z_0ji = 0.05;
-const const_c_0_z = 1;
 const const_k_i = 1;
+const const_c_0_z = 1;
+const const_ro = 1.25;
+const const_p = 0.2;
+const KINEMATIC_VISCOSITY = 0.000015;
+const const_c_p0 = 1.5;
+const PSI_LYAMDA_0 = 1;
 
 let fee_II = fee_II_Enter > 0 ? fee_II_Enter : 30;
 
@@ -37,34 +44,70 @@ let Ep_Exit = + Ep.toFixed(3);
 let Eq_Exit = + Eq.toFixed(3);
 
 // Расчет ветровой нагрузки
-function getWind(supportHigt) {
-    let k_r = 0.19 * Math.pow(supportHigt / const_z_0ji, 0.07);
+function getWind(supportHigt, supportDiametr) {
+    let k_r = 0.19 * Math.pow(const_z_0 / const_z_0ji, 0.07);
     let v_b = const_c_dir * const_c_season * const_v_b0;
-    let c_r_z = k_r * Math.log(supportHigt / const_z_0ji);
+    let c_r_z = k_r * Math.log(supportHigt / const_z_0);
     let v_m_z = c_r_z * const_c_0_z * v_b;
+    let I_v_z = const_k_i / (const_c_0_z * Math.log(supportHigt / const_z_0ji));
+    let q_p_z_e = (1 + 7 * I_v_z) * 0.5 * const_ro * Math.pow(v_m_z, 2);
+    let vz_e = Math.pow(2 * q_p_z_e / const_p, 0.5);
+    let R_e = supportDiametr * vz_e / KINEMATIC_VISCOSITY;
+    let c_pe = const_c_p0 * PSI_LYAMDA_0;
+    let w_e = q_p_z_e * c_pe;
+
+    // перевод из Па в кН/м2
+    let q_p_z_e_translate = q_p_z_e / 1000; /* I dont't use*/
+
+    // перевод в степень
+    let R_e_translate = R_e / 1000000;
 
     // округляем полученные результаты до 3-х знаков
     let k_r_Exit = + k_r.toFixed(3);
     let c_r_z_Exit = + c_r_z.toFixed(3);
     let v_m_z_Exit = + v_m_z.toFixed(3);
+    let I_v_z_Exit = + I_v_z.toFixed(3);
+    let q_p_z_e_Exit = + q_p_z_e.toFixed(3);
+    let q_p_z_e_translate_Exit = + q_p_z_e_translate.toFixed(3); /* I dont't use*/
+    let vz_e_Exit = + vz_e.toFixed(3);
+    let R_e_Exit = + R_e_translate.toFixed(3);
+    let w_e_Exit = + w_e.toFixed(3);
+
+    // перевод в степень
+    let w_e_translate = w_e / 1000;
+    let w_e_translate_Exit = + w_e_translate.toFixed(3);
+
 
     // выводим полученные результаты в HTML
     replaceVariables(const_c_dir, '.c_dir');
     replaceVariables(const_c_season, '.c_season');
     replaceVariables(const_v_b0, '.v_b0');
+    replaceVariables(const_z_0, '.z_0');
     replaceVariables(const_z_0ji, '.z_0ji');
     replaceVariables(const_c_0_z, '.c_0_z');
     replaceVariables(const_k_i, '.k_i');
-
-
+    replaceVariables(const_ro, '.ro');
+    replaceVariables(const_p, '.p');
+    replaceVariables(KINEMATIC_VISCOSITY, '.kinematic_viscosity');
+    replaceVariables(const_c_p0, '.c_p0');
+    replaceVariables(PSI_LYAMDA_0, '.psi_lyambda0');
 
     replaceVariables(supportHigt, '.supportHigt');
     replaceVariables(c_r_z_Exit, '.c_r_z');
     replaceVariables(k_r_Exit, '.k_r');
     replaceVariables(v_b, '.v_b');
     replaceVariables(v_m_z_Exit, '.v_m_z');
+    replaceVariables(I_v_z_Exit, '.I_v_z');
+    replaceVariables(q_p_z_e_Exit, '.q_p_z_e');
+    replaceVariables(vz_e_Exit, '.vz_e');
+    replaceVariables(supportDiametr, '.supportDiametr');
+    replaceVariables(R_e_Exit, '.R_e');
+    replaceVariables(c_pe, '.c_pe');
+    replaceVariables(w_e_Exit, '.w_e');
+    replaceVariables(w_e_translate_Exit, '.w_e_translate_Exit');
+
 }
-getWind(H)
+getWind(H, Dsupport)
 
 // console.log('Сигма_а = ' + sigma_a);
 // console.log('Сигма_а (округление) = ' + sigma_a_Exit);
